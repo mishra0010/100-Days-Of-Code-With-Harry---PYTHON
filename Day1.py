11ebd263566901a0b1ad2e34f296881eb5e82fd4
Harshit Mishra <88685324+mishra0010@users.noreply.github.com>
2022-12-16 17:26:17 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,6 @@
+# Basic Introduction of Python Language 
+# Why to start with Python and Many more
+# Little knowledge about print statement and REPL(Read Evaluate Print Loop)
+
+print("Hello World")
+print(2)
